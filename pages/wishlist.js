d01415940ccda15bf20c4f78f994b918c96b4720
Beth Ashton <Mbeth.ashton@gmail.com>
2019-06-30 10:50:20 -0700

    Quotes stay connected
    A,"Ba,Bb,Bc",C;
    D,E,(Fa,Fb,Fc);
    Input^

--- pages/wishlist.js
+++ pages/wishlist.js
@@ -2,26 +2,61 @@ function printList(input){
   var boxInputs = input.split(';');
   var boxes =[];
   boxInputs.forEach((boxInput, index) =>{
+    //make object
     if (boxInput !== ""){
-      var row = document.createElement('tr');
-      var box = {info: ""}
+      var box = {info: "", array: []}
       
+      var boxArray = boxInput.split(",");
+      //if bits have quotes, find the other quote and combine
+      var temp = []
+      var adding = false;
+      boxArray.forEach((element) => {
+        if (adding === false){
+          if (element.startsWith('"')){
+            console.log("A Quotation mark HAS BEEN FOUND");
+            adding = true;
+            temp.push(element.replace('"', ''))
+          } else {
+            box.array.push(element);
+          }
+        } else {
+          if (adding){
+            if (element.includes('"')){
+              console.log("Closing quote has been found")
+              temp.push(element.replace('"', ''))
+              box.array.push(temp);
+              temp = [];
+              adding = false;
+            } else {
+              temp.push(element)
+            }
+          }  
+        }
+        
+        console.log ("adding = " + adding)
+      })
+      console.log(temp)
+
       //fill object
       box.info = boxInput;
       boxes.push(box)
-
-      //make cells
-      var cell = document.createElement('td');
-
-
-      //fill cells
-      cell.appendChild(document.createTextNode(box.info));
-
-      //append cells
-      row.appendChild(cell);
-
-      outputTbody.appendChild(row);
     }
+  });
+
+  //make table
+  boxes.forEach((box, index) => {
+    var row = document.createElement('tr');
+    
+    //make cells
+    var cell = document.createElement('td');
+
+    //fill cells
+    cell.appendChild(document.createTextNode(box.info));
+
+    //append cells
+    row.appendChild(cell);
+
+    outputTbody.appendChild(row);
   })
   console.log(boxes)
 }
